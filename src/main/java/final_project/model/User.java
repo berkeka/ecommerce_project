1b1e6ba1d815e264860c9d2164df47ce03d3343b
berkeka <berkekalkantr@gmail.com>
2021-06-09 16:13:11 +0300

    Create user model

---
+++ src/main/java/final_project/model/User.java
@@ -0,0 +1,68 @@
+package final_project.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "user_name", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+    
+    public User() {
+    	
+    }
+    
+    public User(String firstName, String lastName, String username, String password) {
+    	setFirstName(firstName);
+    	setLastName(lastName);
+    	setPassword(username);
+    	setUsername(password);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
